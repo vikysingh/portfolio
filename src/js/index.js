6cd807dd0d887a5e0f46b66d08e7675f2c308652
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-09-02 09:46:43 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,35 @@
-import "../styles/style.css"
-alert("ciao")
+// import "../styles/style.css"
+
+const nav = document.querySelector("#nav")
+const navIndicator = document.querySelector("#nav-indicator")
+let open = false
+
+navIndicator.addEventListener("click", () => {
+    if(open) {
+        Navbar.closeNavbar()
+    } else {
+        Navbar.openNavbar()
+    }
+})
+
+for(let i = 0; i<nav.children.length; i++) {
+    nav.children[i].addEventListener("click", () => Navbar.closeNavbar())
+}
+
+class Navbar {
+    static closeNavbar() {
+        nav.style.transition = "300ms"
+        nav.style.transform = "translateX(-100%)"
+        navIndicator.style.transform = "rotate(0deg)"
+
+        open = false
+    }
+
+    static openNavbar() {
+        nav.style.transition = "300ms"
+        nav.style.transform = "translateX(0%)"
+        navIndicator.style.transform = "rotate(90deg)"
+
+        open = true
+    }
+}
