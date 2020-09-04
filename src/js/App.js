4556095e5cf31fcbe77dcc67f3595ac9bc1417ea
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-09-04 09:51:51 +0200

    Add files via upload

--- src/js/App.js
+++ src/js/App.js
@@ -5,13 +5,38 @@ import { Navbar, MapSkills } from "./Classes"
 let navbar = new Navbar("#nav", false, "#nav-indicator")
 let skills = new MapSkills(".skills_body", data.skills, data.classes)
 
+let socialBar = document.querySelector("#social-icons-bar")
+let headerTitle = document.querySelector("#header-title")
+let navIndicator = navbar.navIndicator
+
+let headerTimeline = gsap.timeline()
 
 export default function App() {
     navbar.setNavIndicator()
+    console.log(navbar)
 
     for(let i = 0; i<navbar.nav.children.length; i++) {
         navbar.nav.children[i].addEventListener("click", () => navbar.closeNavbar())
     }
+
+    headerTimeline
+    .to(headerTitle, {
+        duration: 2,
+        display: 'block',
+        transform: 'scale(1)',
+    })
+    .to(headerTitle, {
+        marginTop: '-50px'
+    })
+    .to(navIndicator, {
+        left: '35px',
+        duration: 1,
+        delay: -0.5
+    }, '-=1')
+    .to(socialBar, {
+        right: '5%',
+        duration: 1
+    })
 
     skills.mapSkill()
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import App from "./App"
 import "../styles/style.css"
 
+
 document.addEventListener("DOMContentLoaded", () => {
     App()
 })
