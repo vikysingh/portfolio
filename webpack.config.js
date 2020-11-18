618f6964b5727116059f844b1e6118aeb2a7fa06
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-11-18 18:06:40 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif|PNG)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    },
+    mode: "production",
     entry: "./src/js/index.js",
     output: {
         filename: "main.bundle.js",
         path: path.resolve(__dirname, "build")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|PNG|JPEG|webp)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-env' ]
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+	    minimizer: [
+	        new TerserPlugin({
+	            test: /\.js$/i,
+	            exclude: /node_modules/
+	        })
+	    ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 }
